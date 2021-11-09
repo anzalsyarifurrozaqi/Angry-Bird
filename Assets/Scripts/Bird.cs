523b5ff51ac91623a12989fa04f94b9ba5945e5e
anzalsyarifurrozaqi <46182929+anzalsyarifurrozaqi@users.noreply.github.com>
2021-11-09 18:29:39 +0700

    First Commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Bird : MonoBehaviour {    
+    public enum BirdState { idle, thrown, HitSomething }
+    public GameObject Parent;
+    public Rigidbody2D Rigidbody;
+    public CircleCollider2D Collider;
+
+    public UnityAction OnBirdDestroyed = delegate { };
+    public UnityAction<Bird> OnBirdShot = delegate { };
+
+    public BirdState State { get { return _state; } }
+
+    private BirdState _state;
+    private float _minVelocity = 0.05f;
+    private bool _flagDestroy = false;
+
+    private void Start()
+    {
+        Rigidbody.bodyType = RigidbodyType2D.Kinematic;
+        Collider.enabled = false;
+        _state = BirdState.idle;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_state == BirdState.idle && Rigidbody.velocity.sqrMagnitude >= _minVelocity)
+        {
+            _state = BirdState.thrown;
+        }
+
+        if ((_state == BirdState.thrown || _state == BirdState.HitSomething) && Rigidbody.velocity.sqrMagnitude < _minVelocity && !_flagDestroy)
+        {
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(2));
+        }
+    }
+
+    private IEnumerator DestroyAfter(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Destroy(gameObject);
+    }
+
+    public void MoveTo(Vector2 target, GameObject parent)
+    {
+        gameObject.transform.SetParent(parent.transform);
+        gameObject.transform.position = target;
+    }
+
+    public void Shoot(Vector2 velocity, float distance, float speed)
+    {
+        Collider.enabled = true;
+        Rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        Rigidbody.velocity = velocity * speed * distance;
+        OnBirdShot(this);
+    }
+
+    void OnDestroy()
+    {
+        if (_state == BirdState.thrown || _state == BirdState.HitSomething)
+            OnBirdDestroyed();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _state = BirdState.HitSomething;
+    }
+
+    public virtual void OnTap()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    public BoxCollider2D TapCollider;
+
+    private Bird _shotBird;
+
+    private bool _isGameEnded = false;
+
+    private void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    private void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+
+    private void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
+    }
+
+    private void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0) {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+}
